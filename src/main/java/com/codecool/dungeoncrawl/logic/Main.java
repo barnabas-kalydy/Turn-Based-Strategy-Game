92537b09c0fc116aba1326e4f077477f1909c2c9
Barnabas Kalydy <kalydybarnabas99@gmail.com>
2021-07-31 21:08:51 +0200

    Added theme song.

--- src/main/java/com/codecool/dungeoncrawl/logic/Main.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/Main.java
@@ -3,6 +3,11 @@ package com.codecool.dungeoncrawl.logic;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -11,7 +16,24 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        playMainSong();
+
         new GameLoop().start(primaryStage);
+
+    }
+
+    private void playMainSong() {
+        try {
+            AudioInputStream audioInputStream = AudioSystem
+                    .getAudioInputStream(new File("src/main/resources/music/aoe3_theme_song.wav")
+                            .getAbsoluteFile());
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch(Exception e) {
+            System.out.println("Error with playing sound.");
+            e.printStackTrace();
+        }
     }
 
 }
